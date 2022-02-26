c065284d7425749f20164dee83194ce5238d2907
Sank6 <25014241+Sank6@users.noreply.github.com>
2022-02-26 18:36:21 +0000

    update data

--- scripts/popup.js
+++ scripts/popup.js
@@ -9,4 +9,8 @@ let params = new URLSearchParams(document.location.search);
 (async () => {
     let data = await fetch("./data.json")
     data = await data.json()
+    let relevent_data = data[params.get('website')]
+    for (let key in relevent_data) {
+        document.querySelector(`#${key}`).setAttribute("value", relevent_data[key])
+    }
 })()
