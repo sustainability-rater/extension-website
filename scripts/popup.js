0b861da8f9737510d93bf7da7698b9fcb90ee434
Sank6 <25014241+Sank6@users.noreply.github.com>
2022-02-26 18:43:54 +0000

    overall

--- scripts/popup.js
+++ scripts/popup.js
@@ -6,6 +6,7 @@ let params = new URLSearchParams(document.location.search);
     let relevent_data = data[params.get('website')]
     for (let key in relevent_data) {
         if (key !== "company") document.querySelector(`#${key}`).setAttribute("value", relevent_data[key])
+        if (key === "overall") document.querySelector("#score").textContent = relevent_data[key] * 100
     }
     document.querySelectorAll(".rating-bar").forEach((bar) => {
         let val = bar.getAttribute('value')
